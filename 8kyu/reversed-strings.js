bfa009a0cc5fba310486ad0c877924ecfc03c9c1
unknown <amronglong@gmail.com>
2023-10-26 07:55:18 -0700

    completed codewars challenge

---
+++ 8kyu/reversed-strings.js
@@ -0,0 +1,17 @@
+/*
+Complete the solution so that it reverses the string passed into it.
+
+'world'  =>  'dlrow'
+'word'   =>  'drow'
+
+*/
+function solution(str){
+ if (str.length < 1) return str;
+
+ return str.split('').reverse().join('')
+}
+
+console.log(solution('world')); // =>  'dlrow'
+console.log(solution('hello')); // =>  'olleh'
+// console.log(solution('')); // =>  ''
+// console.log(solution('h')); // =>  'h'
