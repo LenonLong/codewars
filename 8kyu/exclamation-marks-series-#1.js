8b6523aa1a6c7db8ce2096ddc4274e9dd4d40b2f
unknown <amronglong@gmail.com>
2023-11-08 06:33:31 -0800

    completed codewars challenge

---
+++ 8kyu/exclamation-marks-series-#1.js
@@ -0,0 +1,27 @@
+/*
+Exclamation marks series #1: Remove an exclamation mark from the end of string
+Description:
+Remove an exclamation mark from the end of a string. For a beginner kata, you can assume that the input data is always a string, no need to verify it.
+
+Examples
+"Hi!"     ---> "Hi"
+"Hi!!!"   ---> "Hi!!"
+"!Hi"     ---> "!Hi"
+"!Hi!"    ---> "!Hi"
+"Hi! Hi!" ---> "Hi! Hi"
+"Hi"      ---> "Hi"
+*/
+function remove (string) {
+
+    return string.endsWith('!') ? string.slice(0, -1) : string;
+  }
+
+  console.log(remove("Hi!")); // => "Hi"
+  console.log(remove("Hi!!!")); // => "Hi!!"
+  console.log(remove("!Hi")); // => "!Hi"
+  console.log(remove("!Hi!")); // => "!Hi"
+  console.log(remove("Hi! Hi!")); // => "Hi! Hi"
+
+  /*
+  const remove = s => s.replace(/!$/, '');
+  */
