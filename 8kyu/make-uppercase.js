eaf6f3d1974cb059c029ff97ed9f2f51a787ad72
unknown <amronglong@gmail.com>
2023-11-14 06:28:53 -0800

    completed codewars challenge

---
+++ 8kyu/make-uppercase.js
@@ -0,0 +1,8 @@
+/*
+Write a function which converts the input string to uppercase.
+*/
+const makeUpperCase = (str) => {
+    return str.toUpperCase()
+}
+console.log(makeUpperCase("hello"), "HELLO");
+console.log(makeUpperCase("Hello"), "HELLO");
