c07b65df099d47c14e837c1d2bd6b37d16b92409
unknown <amronglong@gmail.com>
2023-12-20 06:24:28 -0800

    completed codewars challenge

---
+++ 8kyu/simple-validation-of-a-username-with-regex.js
@@ -0,0 +1,19 @@
+/*
+Write a simple regex to validate a username. Allowed characters are:
+
+lowercase letters,
+numbers,
+underscore
+Length should be between 4 and 16 characters (both included).
+*/
+function validateUsr(username) {
+    const res =  /^[a-z0-9_]{4,16}$/.test(username)
+    return res;
+  }
+
+    console.log(validateUsr('asddsa'), true);
+    console.log(validateUsr('a'), false);
+    console.log(validateUsr('Hass'), false);
+    console.log(validateUsr('Hasd_12assssssasasasasasaasasasasas'), false);
+    console.log(validateUsr(''), false);
+    console.log(validateUsr('____'), true);
