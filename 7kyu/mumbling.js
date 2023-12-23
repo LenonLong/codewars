5a694157321fd18831f6056139684b483ab33570
unknown <amronglong@gmail.com>
2023-12-23 12:48:43 -0800

    completed codewars challenge

---
+++ 7kyu/mumbling.js
@@ -0,0 +1,20 @@
+/*
+This time no story, no theory. The examples below show you how to write function accum:
+
+Examples:
+accum("abcd") -> "A-Bb-Ccc-Dddd"
+accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+accum("cwAt") -> "C-Ww-Aaa-Tttt"
+The parameter of accum is a string which includes only letters from a..z and A..Z.
+*/
+function accum(str) {
+    let strRepeat = str.split('').map((char, index) => {
+        return char.toUpperCase() + char.toLowerCase().repeat(index);
+
+    })
+    return strRepeat.join('-')
+}
+console.log(accum("abc")) // => A-Bb-Ccc
+console.log(accum("ZpglnRxqenU")); // => , "Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu"
+console.log(accum("NyffsGeyylB")); // => , "N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyyy-Llllllllll-Bbbbbbbbbbb"
+console.log(accum("MjtkuBovqrU")); // => , "M-Jj-Ttt-Kkkk-Uuuuu-Bbbbbb-Ooooooo-Vvvvvvvv-Qqqqqqqqq-Rrrrrrrrrr-Uuuuuuuuuuu"
