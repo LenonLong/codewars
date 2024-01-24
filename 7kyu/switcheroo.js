384945dfa27907905f3ecad99a499f81b508f999
unknown <amronglong@gmail.com>
2024-01-24 09:01:07 -0800

    completed codewars challenge

---
+++ 7kyu/switcheroo.js
@@ -0,0 +1,28 @@
+/*
+Given a string made up of letters a, b, and/or c, switch the position of letters a and b (change a to b and vice versa). Leave any incidence of c untouched.
+
+Example:
+
+'acb' --> 'bca'
+'aabacbaa' --> 'bbabcabb'
+*/
+
+const switcheroo = x => {
+    let result = '';
+
+    for (let i = 0; i < x.length; i++) {
+        if (x[i] === 'a') {
+            result += 'b';
+        }else if (x[i] === 'b') {
+            result += 'a';
+        } else {
+            result += x[i]
+        }
+        
+    }
+    return result
+}
+
+console.log(switcheroo('abc'));             // =>  'bac'
+console.log(switcheroo('aaabcccbaaa'));     // =>  'bbbacccabbb'
+console.log(switcheroo('ccccc'));           // =>  'ccccc'
