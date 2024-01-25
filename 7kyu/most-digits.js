bd2ba2fb1a6dc71c8821ea8918ba5325313971b4
unknown <amronglong@gmail.com>
2024-01-25 11:59:04 -0800

    completed codewars challenge

--- 7kyu/most-digits.js
+++ 7kyu/most-digits.js
@@ -5,8 +5,15 @@ If two numbers in the argument array have the same number of digits, return the 
 
 */
 function findLongest(array){
-  return Math.max(...array)
+ let longestNumber = array[0];
+
+ for (let i = 1; i < array.length; i++) {
+    if (array[i].toString().length > longestNumber.toString().length) {
+        longestNumber = array[i];
+    }
   }
+  return longestNumber
+}
   console.log(findLongest([1, 10, 100]), 100)
   console.log(findLongest([9000, 8, 800]), 9000)
   console.log(findLongest([8, 900, 500]), 900)
