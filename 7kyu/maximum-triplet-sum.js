226fe75f01a84b833b8e8283dd059650721a13c1
unknown <amronglong@gmail.com>
2024-03-28 08:06:10 -0700

    completed codewars challenge

---
+++ 7kyu/maximum-triplet-sum.js
@@ -0,0 +1,47 @@
+/*
+Maximum Triplet Sum (Array Series #7):
+Task:
+Given an array/list [] of n integers , find maximum triplet sum in the array Without duplications .
+
+Notes :
+- Array/list size is at least 3 .
+- Array/list numbers could be a mixture of positives , negatives and zeros .
+- Repetition of numbers in the array/list could occur , So (duplications are not included when summing).
+
+Input >> Output Examples: 
+
+1- maxTriSum ({3,2,6,8,2,3}) ==> return (17)
+Explanation:
+- As the triplet that maximize the sum {6,8,3} in order , their sum is (17)
+- Note : duplications are not included when summing , (i.e) the numbers added only once .
+
+2- maxTriSum ({2,1,8,0,6,4,8,6,2,4}) ==> return (18)
+Explanation:
+- As the triplet that maximize the sum {8, 6, 4} in order , their sum is (18) ,
+- Note : duplications are not included when summing , (i.e) the numbers added only once .
+
+3- maxTriSum ({-7,12,-7,29,-5,0,-7,0,0,29}) ==> return (41)
+Explanation:
+- As the triplet that maximize the sum {12 , 29 , 0} in order , their sum is (41) ,
+- Note : duplications are not included when summing , (i.e) the numbers added only once .
+*/
+const maxTriSum = (numbers) => {
+    let result = 0;
+    let sortArr = numbers.sort((a, b) => b - a);
+    let filterDouble = sortArr.filter((v, i, self) => i === self.indexOf(v)) 
+    
+    result += filterDouble[0] + filterDouble[1] + filterDouble[2]
+
+    return result
+}
+console.log(maxTriSum([3,2,6,8,2,3]),17);
+console.log(maxTriSum([2,9,13,10,5,2,9,5]),32);
+console.log(maxTriSum([2,1,8,0,6,4,8,6,2,4]),18);
+console.log(maxTriSum([-3,-27,-4,-2,-27,-2]),-9);
+
+/*
+const maxTriSum = numbers => {
+  const [a,b,c,...rest] = [...new Set([...numbers])].sort((a,b)=>b-a)
+  return a+b+c
+}
+*/
